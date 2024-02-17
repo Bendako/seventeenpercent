ad4a9d5c7021246704e97842ac4df5dee312df80
Bendako <62848655+Bendako@users.noreply.github.com>
2024-02-17 02:32:02 +0200

    add form

--- src/app/Futureride/page.tsx
+++ src/app/Futureride/page.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
+import RideForm from '../components/RideForm'
 
 type Props = {}
 
 const FutureRide = (props: Props) => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
-      <p className="text-4xl animate-bounce m-4">Future Ride Page</p>
-      {/* <h3 className="text-3xl">(17%)</h3> */}
-      {/* <p className="animate-bounce m-4">
-        Gonna be here fucking app soon...
-      </p> */}
-    </div>
+    <>
+      <RideForm />
+    </>
   )
 }
 
