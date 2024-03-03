33780d651627b9f284dc022d06bddad75e3e122f
Bendako <62848655+Bendako@users.noreply.github.com>
2024-03-03 02:37:09 +0200

    remove dropdown menu and refactor and refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,15 @@ import Image from "next/image";
 import Card from "./components/Card";
 
 export default function Home() {
+
+  const numberOfCards: number = 10;
+
+
   return (
     <div className="container p-10 h-screen flex flex-col items-center justify-center">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {[...Array(numberOfCards)].map((_, index) => (
+        <Card key={index} />
+      ))}
     </div>
   );
 }
